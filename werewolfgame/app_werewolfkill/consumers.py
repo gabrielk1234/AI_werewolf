cf04872a95abeef99053a790aa5333f8a1545327
gabriel <F113118131@nkust.edu.tw>
2025-05-22 11:31:32 +0800

    working on website 1

--- werewolfgame/app_werewolfkill/consumers.py
+++ werewolfgame/app_werewolfkill/consumers.py
@@ -1,23 +1,33 @@
 # your_app/consumers.py
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import os
+import yaml
 import asyncio
-from app_werewolfkill.game_logic.test import night
+from app_werewolfkill.game_logic.test import GameMaster
 
 class Myconsumer(AsyncWebsocketConsumer):
+    def load_messages(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_dir, "game_logic/message.yaml")
+
+        # Load Message
+        with open(file_path, "r", encoding="utf-8") as f:
+            self.messages = yaml.safe_load(f)
+
     async def connect(self):
         await self.accept()
+        self.load_messages()
+        self.gm = GameMaster(self.messages)
 
     async def disconnect(self, close_code):
         print("WebSocket disconnected")
 
     async def receive(self, text_data):
-        # 收到前端的啟動訊號後，回傳姓名
-        if text_data == "start":
-            msg = night()
-            await self.send(text_data=json.dumps({
-                'type':'message',
-                'value':msg
-            }))
-
+        # Start Game
+        if text_data == "game_start":
+            msg_to_send = self.gm.game_start()
+            for msg in msg_to_send:
+                await self.send(text_data=json.dumps(msg))
+                await asyncio.sleep(2)
     
--- werewolfgame/app_werewolfkill/game_logic/test.py
+++ werewolfgame/app_werewolfkill/game_logic/test.py
@@ -10,26 +10,23 @@ from app_werewolfkill.game_logic.werewolves_character import WerewolfCharacter
 from app_werewolfkill.game_logic.moderator import Moderator
 from app_werewolfkill.game_logic.utils import extract_json
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "message.yaml")
+class GameMaster():
+    def __init__(self,messages):
+        self.wolf1 = WerewolfCharacter(name="Alex", role="werewolf", teammate="Anson")
+        self.wolf2 = WerewolfCharacter(name="Anson", role="werewolf", teammate="Alex")
+        self.villager1 = WerewolfCharacter(name="Bob", role="villager")
+        self.villager2 = WerewolfCharacter(name="Charlie", role="villager")
+        self.seer = WerewolfCharacter(name="Diana", role="seer")
+        self.witch = WerewolfCharacter(name="Eve", role="witch")
 
-# Load Message
-with open(file_path, "r", encoding="utf-8") as f:
-    messages = yaml.safe_load(f)
+        self.moderator = Moderator([self.wolf1, self.wolf2, self.villager1, self.villager2, self.seer, self.witch])
+        self.messages = messages
 
-# Initialize Character
-wolf1 = WerewolfCharacter(name="Alex", role="werewolf", teammate="Anson")
-wolf2 = WerewolfCharacter(name="Anson", role="werewolf", teammate="Alex")
-villager1 = WerewolfCharacter(name="Bob", role="villager")
-villager2 = WerewolfCharacter(name="Charlie", role="villager")
-seer = WerewolfCharacter(name="Diana", role="seer")
-witch = WerewolfCharacter(name="Eve", role="witch")
-
-moderator = Moderator([wolf1, wolf2, villager1, villager2, seer, witch])
-
-def night():
-    moderator.set_night_status()
-    use_heal = False
-    killed_dict = {}
-    # Day and Night Cycle
-    return(messages['night'] + "今天是第" + str(moderator.night) + "晚")
+    def game_start(self):
+        messages_to_send = [
+            {'type': 'system', 'value': self.messages['game_start']},
+            {'type': 'system', 'value': self.messages['distribute_chac']}
+        ]
+        
+        # ✅ 回傳前端訊息
+        return messages_to_send
